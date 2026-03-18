fd32341b71503ef1e21b085f5884f9c2ed8c8ee9
Maksim Demidov <maaksaa@gmail.com>
2026-03-18 18:51:59 +0100

    add layer

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@ import 'maplibre-gl/dist/maplibre-gl.css'
 const container = ref<HTMLElement>()
 let map: maplibregl.Map | null = null
 
+// координаты слоя над городом
+const BOUNDS = {
+  west: 19.65,
+  south: 45.18,
+  east: 20.02,
+  north: 45.36,
+}
+
+const outerRing: [number, number][] = [
+  [BOUNDS.west, BOUNDS.south],
+  [BOUNDS.east, BOUNDS.south],
+  [BOUNDS.east, BOUNDS.north],
+  [BOUNDS.west, BOUNDS.north],
+  [BOUNDS.west, BOUNDS.south],
+]
+
 onMounted(() => {
   map = new maplibregl.Map({
     container: container.value!,
@@ -23,6 +39,31 @@ onMounted(() => {
       trackUserLocation: true,
     }),
   )
+
+  // доп слой поверх карты
+  map.on('load', () => {
+    map!.addSource('fog', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Polygon',
+          coordinates: [outerRing],
+        },
+      },
+    })
+
+    map!.addLayer({
+      id: 'fog-layer',
+      type: 'fill',
+      source: 'fog',
+      paint: {
+        'fill-color': '#000000',
+        'fill-opacity': 0.6,
+      },
+    })
+  })
 })
 
 onUnmounted(() => {
